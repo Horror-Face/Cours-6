348e1040924d99490a69932073e346d9581420eb
Horror Face <raphaelds305@gmail.com>
2025-09-22 12:46:15 -0400

    Update While.java

--- Java6/While.java
+++ Java6/While.java
@@ -3,8 +3,16 @@ package Java6;
 import java.util.Scanner;
 
 public class While {
-public static void main(String[] args) {
-    Scanner sc = new Scanner ( System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-}
+        int a = 10;
+
+        while (a > 0) {
+            System.out.printf(" La valeur A est %d %n", a);
+            a = a - 1;
+        }
+        System.out.println("A est plus petit");
+        sc.close();
+    }
 }
