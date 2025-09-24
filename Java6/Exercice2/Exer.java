50b867665403b52a5afab452397f9481f4c29ad3
Horror Face <raphaelds305@gmail.com>
2025-09-24 10:45:21 -0400

    Update Exer.java

--- Java6/Exercice2/Exer.java
+++ Java6/Exercice2/Exer.java
@@ -1,3 +1,6 @@
+//Raphaël Desjardins
+//2025-09-24
+//TP 2
 package Java6.Exercice2;
 
 import java.util.Scanner;
@@ -5,21 +8,31 @@ import java.util.Scanner;
 public class Exer {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double pie = 3.14;
-         Scanner scanner = new Scanner(System.in);
-        int choix;
+        double pi = 3.14;
+        Scanner scanner = new Scanner(System.in);
+        String choix;
         do {
-            choix = 0;
-            System.out.println("1. Périmètre et diametre d'un cercle\n2. Périmetre d'un triangle rectangle\n3. Ciao, bye! C’est le temps de faire le sommaire final");
+            choix = "";
+            System.out.println(
+                    "a. Périmètre et diametre d'un cercle\nb. Périmetre d'un triangle rectangle\nc. Ciao, bye! C'est le temps de faire le sommaire final");
             System.out.print("Votre choix : ");
-            choix = scanner.nextInt(); // la variable de contrôle est mise à jour
-            if (choix == 1)   
-            {
-                System.out.print("Entrer un rayon:");
-                
+            choix = scanner.next(); // la variable de contrôle est mise à jour
+            switch (choix) {
+                case "a":
+                    System.out.print("Entré un rayon: ");
+                    double ray = sc.nextDouble();
+                    double dia = ray * 2;
+                    double peri = (double) (2 * pi * ray);
+                    Math.round(peri);
+                    System.out.printf("Le prérimètre est de %f et le diamètre est de %f", peri, dia);
+
             }
 
-        } while (choix != 3);
-        System.out.println("merci d'avoir quitter");      
+        }
+
+        while (choix != "c");
+        System.out.println("merci d'avoir quitter");
+        sc.close();
+        scanner.close();
     }
 }
