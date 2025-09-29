a517f7fdce07c231df056d36e05710f9a26c4526
Horror Face <raphaelds305@gmail.com>
2025-09-29 13:17:53 -0400

    Update Exer.java

--- Java6/Exercice2/Exer.java
+++ Java6/Exercice2/Exer.java
@@ -8,23 +8,32 @@ import java.util.Scanner;
 public class Exer {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double pi = 3.14;
         Scanner scanner = new Scanner(System.in);
         String choix;
         do {
             choix = "";
-            System.out.println(
-                    "a. Périmètre et diametre d'un cercle\nb. Périmetre d'un triangle rectangle\nc. Ciao, bye! C'est le temps de faire le sommaire final");
+            System.out.printf(
+                    "%na. Périmètre et diametre d'un cercle\nb. Périmetre d'un triangle rectangle\nc. Ciao, bye! C'est le temps de faire le sommaire final%n");
             System.out.print("Votre choix : ");
             choix = scanner.next(); // la variable de contrôle est mise à jour
             switch (choix) {
                 case "a":
-                    System.out.print("Entré un rayon: ");
-                    double ray = sc.nextDouble();
-                    double dia = ray * 2;
-                    double peri = (double) (2 * pi * ray);
-                    Math.round(peri);
-                    System.out.printf("Le prérimètre est de %f et le diamètre est de %f", peri, dia);
+                    // double ray;
+                    do {
+                        System.out.print("Entré un rayon: ");
+                        double ray = sc.nextDouble();
+                        if (saisie.hasNextDouble) {
+
+                        }
+                        double dia = ray * 2;
+                        double peri = (double) (2 * Math.PI * ray);
+                        Math.round(peri);
+                        System.out.printf("Le prérimètre est de %.3f et le diamètre est de %.2f", peri, dia);
+                    }
+
+                    while (sc.hasNextDouble());
+
+                case "b":
 
             }
 
